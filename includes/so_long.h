d2dd110425604075ad2b0a1850481a05b4f0340e
Alan <alan_pires2@hotmail.com>
2021-08-29 14:14:02 -0300

    initial struct

--- includes/so_long.h
+++ includes/so_long.h
@@ -3,4 +3,36 @@
 #include "../mlx_linux/mlx.h"
 #include "../libft/libft.h"
 
+typedef struct s_window
+{
+	void	*ref;
+	int		x;
+	int		y;
+}	t_window;
+
+typedef struct	s_sprite
+{
+	void	*ref;
+	int		width;
+	int		height;
+	char	*pixels;
+	int		bpp;
+	int		line_size;
+	int		endian;
+	//int	posx;
+	//int	posy;
+}	t_sprite;
+
+typedef struct	s_game
+{
+	void		*mlx;
+	t_window	window;
+	t_sprite	player;
+	t_sprite	collect;
+	t_sprite	wall;
+	t_sprite	empty;
+
+}	t_game;
+
+
 # endif
